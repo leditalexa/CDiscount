a37ceab88445247071a0d015060ae8c7e01abe89
Arthur Le Dain <ledainarth@eisti.eu>
2015-03-07 23:05:54 +0100

    Multi langue front && cookie user ope

---
+++ WebContent/js/app-controllers.js
@@ -0,0 +1,282 @@
+'use strict';
+
+var app = angular.module("BestWinesApp")
+
+  
+
+.controller("HomeCtrl", ["$scope", "$window", "$location", "$http", "$cookieStore", "translationService", "UrlProvider", "UserService",
+                         function($scope, $window, $location, $http, $cookieStore, translationService, UrlProvider, User) {
+	
+
+	
+	translationService.getTranslation($scope, translationService.lang); 
+	$scope.$watch(function(){return translationService.lang;}, function(newValue, oldValue) {
+		translationService.getTranslation($scope, translationService.lang); 
+		
+	}, true);
+
+	$scope.isSubscribing = false;
+	
+	$scope.subscribe = function(bool){
+		$scope.isSubscribing = bool;
+	};
+	
+	$scope.User = User;
+	
+	$scope.userDatas = {
+	  	identifiant : "",
+	  	password : "",
+	  	age : 0,
+	  	firstname : "",
+	  	lastname : "",
+	  	zip : "",
+	  	lang : translationService.lang
+	  	
+  	};
+	
+	
+	
+	$scope.onLogin = function(){
+		$http.post(UrlProvider.SERVICE_USER,$scope.userDatas).success(function(data){
+			if(data.code==0){
+				$scope.User.isLogged = true;
+				$scope.User.datas = data.content;
+				$scope.setUserCookies();
+				$location.url(UrlProvider.WINES);
+			}else{
+				alert(data.message);
+			}
+		});
+	};
+	
+	$scope.loginByCookie = function(){
+		var cookie = $cookieStore.get("UserCookie");
+		if(cookie){
+			$http.post(UrlProvider.SERVICE_USER,cookie).success(function(data){
+				if(data.code==0){
+				
+					$scope.User.isLogged = true;
+					$scope.User.datas = cookie;
+					$scope.userDatas = cookie;
+					translationService.lang = cookie.lang;
+
+				}else{
+					alert(data.message);
+				}
+			});
+			
+		}
+	};
+	
+	$scope.onSignup = function(){
+		$http.put(UrlProvider.SERVICE_USER,$scope.userDatas).success(function(data){
+			if(data.code==0){
+				$scope.User.isLogged = true;
+				$scope.User.datas = $scope.userDatas;
+				$scope.setUserCookies();
+				$location.url(UrlProvider.WINES);
+			}else{
+				alert(data.message);
+			}
+		});
+	};
+	
+	$scope.onLogout = function(){
+		$scope.userDatas = {
+			  	identifiant : "",
+			  	password : "",
+			  	age : 0,
+			  	firstname : "",
+			  	lastname : "",
+			  	zip : "",
+			  	lang : translationService.lang
+		  	};
+		$scope.User.isLogged = false;
+		$scope.rmUserCookies();
+	};
+	
+	
+	
+
+	
+	
+	
+	
+	$scope.setUserCookies = function(){
+		$cookieStore.put("UserCookie",$scope.User.datas);
+	};
+	
+	$scope.rmUserCookies = function(){
+		$cookieStore.remove("UserCookie");
+	};
+	
+	$scope.loginByCookie();
+}])
+
+
+    
+.controller("WinesCtrl", ["$scope",  "$window", "$location", "$http", "$anchorScroll", "translationService", 
+                          "Meals", "Wines", "carrouselService", "UserService", "UrlProvider",
+                          function($scope, $window, $location, $http, $anchorScroll, translationService,
+                        		  Meals, Wines, carrouselService, User, UrlProvider) {
+	
+	$scope.scrollTo = function(id) {
+	    $location.hash(id);
+	    $anchorScroll();
+	  };
+
+	
+	translationService.getTranslation($scope, translationService.lang); 
+	$scope.$watch(function(){return translationService.lang;}, function(newValue, oldValue) {
+		translationService.getTranslation($scope, translationService.lang); 
+	}, true);
+
+	
+	$scope.Meals = Meals;
+	$scope.Wines = Wines;
+	$scope.User = User;
+	
+	/* Requests */
+	$scope.getWineFrom = function(url){
+		$scope.wines = [];
+		$scope.init = false;
+		$scope.showGIF = true;	
+		$scope.wineSearchNumber = $scope.WINESEARCHNUMBER;
+		$http.get(url).success(function(data){
+			$scope.wines = data.content;
+			$scope.showGIF = false;
+		}).error(function (data){
+			$scope.error = data;
+			$scope.showGIF = false;
+		});
+	};
+	
+	$scope.doMealCarrouselSearch = function(){
+		$scope.getCarrouselFrom(UrlProvider.SERVICE_FIND_RECIPE+$scope.mealSearch);
+	};
+	
+	$scope.doWineKeywordSearch = function(){
+		$scope.getWineFrom(UrlProvider.SERVICE_FIND_WINE+$scope.wineSearch);
+		$scope.scrollTo("winemarker");
+		
+	};
+	
+	$scope.doWineFromMealCarrousel = function(meal){
+		$scope.mealSearch = meal.title;
+		$scope.getWineFrom(UrlProvider.SERVICE_ASSOCIATE_WINE+$scope.mealSearch);
+		$scope.scrollTo("winemarker");
+	};
+	
+	/* Requests */
+	
+	
+	/* Meal form */
+	$scope.mealSearch = "";
+	/* Meal form */
+	
+	/* carrousel for meal search */
+	carrouselService.getCarrouselUtils($scope);
+	/* carrousel for meal search */
+	
+	/* wine form */
+	$scope.wineSearch = "";
+	
+	$scope.wine_price = {
+			min:0,
+			max:300
+	};
+	/* wine form */
+	
+
+	/* tri des vins */
+	$scope.wineCriterion = "price";
+	$scope.wineOrdering = "";
+	
+	
+	/* wine output */
+	$scope.init = true;
+	
+	$scope.showGIF = false;
+	
+	$scope.wines = [];
+	
+	$scope.WINESEARCHNUMBER = 12;
+	$scope.wineSearchNumber = $scope.WINESEARCHNUMBER;
+	
+	$scope.wineSearchNumberIncrement = 4;
+	
+	$scope.loadMoreWineSearchResult = function(){
+		$scope.wineSearchNumber += $scope.wineSearchNumberIncrement;
+	};
+	
+	$scope.noWineSearchResult = function(){
+		return ($scope.wines.length==0);	
+	};
+	
+	
+	$scope.showLoadMoreWineSearchResult = function(){
+		return ($scope.wineSearchNumber < $scope.wines.length);	
+	};
+	/* wine output */
+	
+
+	
+}])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+.controller("NavCtrl", ["$scope", "$location","$cookieStore", "UrlProvider","translationService", "UserService",
+                        function($scope,$location,$cookieStore, UrlProvider , translationService, User) {	
+	
+	translationService.getTranslation($scope, translationService.lang); 
+	$scope.$watch(function(){return translationService.lang;}, function(newValue, oldValue) {
+		translationService.getTranslation($scope, translationService.lang); 
+	}, true);
+
+	
+	$scope.setLang = function(lang){
+		translationService.lang = lang;
+		$scope.User.datas.lang = lang;
+		$cookieStore.put("UserCookie",$scope.User.datas);
+	};
+	
+	
+	$scope.User = User;
+	
+	$scope.loadHome = function () {
+	    $location.url(UrlProvider.HOME);
+	};
+	
+	
+	$scope.loadWines = function () {
+	    $location.url(UrlProvider.WINES);
+	};
+		
+	
+	 $scope.isActive = function (viewLocation) { 
+		 return viewLocation === $location.path();
+	 };
+	       
+}])
+
+
+;
+
---
+++ WebContent/js/app-factories.js
@@ -0,0 +1,103 @@
+'use strict';
+
+var app = angular.module("BestWinesApp")
+
+
+.factory('UserService', [function() {
+  var sdo = {
+	  datas : {
+		  	identifiant : "",
+		  	password : "",
+		  	age : 0,
+		  	firstname : "",
+		  	lastname : "",
+		  	zip : "",
+		  	lang : ""
+	  		},
+      isLogged : false
+  };
+  return sdo;
+  
+}])
+
+
+
+
+
+/* Wine Functions */
+.factory("Wines", function ($window) {  
+	
+	var Wine = {};
+	
+	Wine.getWineTitle = function(wine){
+		return new Array(wine.title);
+	};
+	
+	Wine.getWineIcon = function(wine){
+		if(wine.url_icon){
+			return wine.url_icon; 
+		}else{
+			return home_url+"img/not_available_icon.jpg";
+		}
+		
+	};
+		
+	Wine.getWinePrice = function(wine){
+		return new Array(wine.price);
+	};
+	
+	Wine.getWineRating = function(wine){
+		return new Array(wine.rating);
+	};
+	
+	Wine.getWineDescription = function(wine){
+		return new Array(wine.description);
+	};	
+	
+
+	Wine.goToWines = function(wine){
+		$window.open(wine.url);
+	};
+		
+	return Wine;
+	
+})
+
+
+/* Meal Functions */
+.factory("Meals", function ($window) {  
+	
+	var Meal = {};
+	
+
+	
+	Meal.getMealDifficulty = function(meal){
+		return new Array(meal.difficulty);
+	};
+	
+	Meal.getMealPrice = function(meal){
+		return new Array(meal.cost);
+	};
+	
+	Meal.getMealIcon = function(meal){
+		if(meal.pictures && meal.pictures[2] && meal.pictures[2].url){
+			return meal.pictures[2].url; 
+		}else{
+			return home_url+"img/not_available_icon.jpg";
+		}
+		
+	};
+	
+	Meal.getMealRating = function(meal){
+		return new Array(meal.rating);
+	};
+	
+	
+	Meal.goToRecipe = function(meal){
+		$window.location.href = "http://www.marmiton.org/recettes/recette_"+(meal.title.replace(" ","-"))+"_"+meal.id+".aspx";
+	};
+	
+
+	return Meal;
+	
+});
